d731468373c38296b8ade046f0aa0e99c34b47b4
Maxime-Derri <maxime.derri@etu.sorbonne-universie.fr>
2022-11-22 14:25:58 +0100

    possible solution

--- TME6/src/main_wait.cpp
+++ TME6/src/main_wait.cpp
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <signal.h>
 
+bool is_child = false;
+
 
 //avec wait (non waitpid)
 int wait_till_pid1(pid_t pid) {
@@ -42,48 +44,61 @@ int wait_till_pid2(pid_t pid, int sec) { //pas a faire ~ en rab
 
 void handler_alrm(int sig) {
     std::cout << "handler: fin tempo" << std::endl;
+    is_child = false;
 }
 
 void handler_chd(int sig) {
     //rien: provoquer la reception du signal (non ignoré)
     std::cout << "handler: child" << std::endl;
+    is_child = true;
 }
 
 //utiliser alarm(), signal et les masques de signaux
 int wait_till_pid3(pid_t pid, int sec) { //pb
-    //sigset_t old;
+    sigset_t old;
     sigset_t sig;
     sigfillset(&sig);
 
-    //sigprocmask(SIG_SETMASK, &sig, &old);
+    sigprocmask(SIG_SETMASK, &sig, &old);
 
     sigdelset(&sig, SIGALRM);
     sigdelset(&sig, SIGCHLD);
     signal(SIGALRM, handler_alrm);
     signal(SIGCHLD, handler_chd);
     alarm(sec);
-    sigsuspend(&sig); //on peut avoir recu sigchild, et entre sigsuspend et sigpending sigalrm ?
-                      //sigsuspend: handler déclanché mais signaux pas reset ?
+    sigsuspend(&sig); //on peut avoir recu sigchild, et entre sigsuspend et sigpending sigalrm ? SIGPROCMASK regle le probleme (le mask est modifié seulement en sigsuspend et avant/apres tt est masqué)?
+                      //sigsuspend: handler déclanché mais signaux pas reset ? seul solution: var global ? cafr sigsuspend retire du mask les 2 signaux -> traitement retour mode U
                       //=> provoque les erreurs lignes qui suivent ?
 
-    //reveil
     sigset_t rec;
     sigpending(&rec);
-    //if(sigismember(&sig, SIGCHLD)) { //ne fonctionne pas avec SIGCHLD ?
-    int test = sigismember(&rec, SIGCHLD); //fonctionne pas non plus
+    if(is_child) {
+        alarm(0);
+        signal(SIGALRM, SIG_DFL);
+        sigprocmask(SIG_SETMASK, &old, nullptr);
+        return pid;
+    }
+    else {
+        signal(SIGCHLD, SIG_DFL);
+        sigprocmask(SIG_SETMASK, &old, nullptr);
+        return 0;
+    }
+    /*
+    int test = sigismember(&rec, SIGCHLD);
     if(test == -1) {
         perror("sigpending / sigismember");
         return -1;
     }
     else if(test) { //child
         alarm(0);
-        signal(SIGALRM, SIG_DFL); //tj exec...
+        signal(SIGALRM, SIG_DFL); //tj exec... utlisier une var global depuis les handers ?
         return pid;
     }
     else {
         signal(SIGCHLD, SIG_DFL);
         return 0;
     }
+    */
 
 }
 
@@ -102,7 +117,7 @@ int main(int argc, char **argv) {
         pid_t res;
         //res = wait_till_pid1(child);
         //res = wait_till_pid2(child, 4);
-        res = wait_till_pid3(child, 5);
+        res = wait_till_pid3(child, 3);
         if(res == -1) {
             std::cout << "pas bien" << std::endl;
             return EXIT_FAILURE;
