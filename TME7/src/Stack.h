c0ab2d224de9e4954c53b9391614133f1e758b99
Maxime-Derri <maxime.derri@etu.sorbonne-universie.fr>
2022-11-28 17:24:10 +0100

    exo sur la pile à moitié

--- TME7/src/Stack.h
+++ TME7/src/Stack.h
@@ -1,6 +1,17 @@
 #pragma once
 
 #include <cstring> // size_t,memset
+#include <condition_variable>
+#include <mutex>
+#include <semaphore.h>
+#include <iostream>
+
+
+/*
+Ecrivez progressivement une version avec N pro ducteurs et M consommateurs, qui
+utilise un segment de mémoire partagée nommé, et se termine proprement sur Ctrl-C
+*/
+
 
 namespace pr {
 
@@ -10,18 +21,51 @@ template<typename T>
 class Stack {
 	T tab [STACKSIZE];
 	size_t sz;
+
+	sem_t vide;
+	sem_t full;
+	sem_t sync;
+
 public :
-	Stack () : sz(0) { memset(tab,0,sizeof tab) ;}
+	Stack () : sz(0) {
+		memset(tab,0,sizeof tab) ;
+		if(sem_init(&vide, 1, STACKSIZE) < 0) {
+			perror("sem_init");
+			exit(1);
+		}
+
+		if(sem_init(&full, 1, 0) < 0) {
+			perror("sem_init");
+			exit(1);
+		}
+
+		if(sem_init(&sync, 1, 1)) {
+			perror("sem_init");
+			exit(1);
+		}
+	}
 
 	T pop () {
 		// bloquer si vide
+		sem_wait(&full);
+
+		sem_wait(&sync);
 		T toret = tab[--sz];
+		sem_post(&sync);
+
+		sem_post(&vide);
 		return toret;
 	}
 
 	void push(T elt) {
 		//bloquer si plein
+		sem_wait(&vide);
+
+		sem_wait(&sync);
 		tab[sz++] = elt;
+		sem_post(&sync);
+
+		sem_post(&full);
 	}
 };
 
--- TME7/src/prod_cons.cpp
+++ TME7/src/prod_cons.cpp
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <vector>
+#include <sys/mman.h>
 
 
 using namespace std;
@@ -12,19 +13,23 @@ void producteur (Stack<char> * stack) {
 	char c ;
 	while (cin.get(c)) {
 		stack->push(c);
+		cout << c << " ===>" << std::endl;
 	}
 }
 
 void consomateur (Stack<char> * stack) {
 	while (true) {
 		char c = stack->pop();
-		cout << c << flush ;
+		cout << "<=== " << c << std::endl;//flush ;
 	}
 }
 
 int main () {
-	Stack<char> * s = new Stack<char>();
+	//Stack<char> * s;// = new Stack<char>();
+	Stack<char> *s =(Stack<char> *)mmap(0, sizeof(Stack<char>), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+	s = new Stack<char>;
 
+	
 	pid_t pp = fork();
 	if (pp==0) {
 		producteur(s);
@@ -41,6 +46,7 @@ int main () {
 	wait(0);
 
 	delete s;
+	munmap(s, sizeof(*s));
 	return 0;
 }
 
